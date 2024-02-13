99b87bc344c4f503fcc21da15db724af8f380c02
Luis Ortiz <luis.ortiz7905@gmail.com>
2024-02-12 21:21:19 -0400

    Adding some changes to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,101 +3,107 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      {/* <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    // <main className={styles.main}>
+    //   {/* <div className={styles.description}>
+    //     <p>
+    //       Get started by editing&nbsp;
+    //       <code className={styles.code}>app/page.tsx</code>
+    //     </p>
+    //     <div>
+    //       <a
+    //         href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         By{" "}
+    //         <Image
+    //           src="/vercel.svg"
+    //           alt="Vercel Logo"
+    //           className={styles.vercelLogo}
+    //           width={100}
+    //           height={24}
+    //           priority
+    //         />
+    //       </a>
+    //     </div>
+    //   </div>
+
+    //   <div className={styles.center}>
+    //     <Image
+    //       className={styles.logo}
+    //       src="/next.svg"
+    //       alt="Next.js Logo"
+    //       width={180}
+    //       height={37}
+    //       priority
+    //     />
+    //   </div>
+
+    //   <div className={styles.grid}>
+    //     <a
+    //       href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //       className={styles.card}
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       <h2>
+    //         Docs <span>-&gt;</span>
+    //       </h2>
+    //       <p>Find in-depth information about Next.js features and API.</p>
+    //     </a>
+
+    //     <a
+    //       href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //       className={styles.card}
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       <h2>
+    //         Learn <span>-&gt;</span>
+    //       </h2>
+    //       <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+    //     </a>
+
+    //     <a
+    //       href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //       className={styles.card}
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       <h2>
+    //         Templates <span>-&gt;</span>
+    //       </h2>
+    //       <p>Explore starter templates for Next.js.</p>
+    //     </a>
+
+    //     <a
+    //       href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //       className={styles.card}
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       <h2>
+    //         Deploy <span>-&gt;</span>
+    //       </h2>
+    //       <p>
+    //         Instantly deploy your Next.js site to a shareable URL with Vercel.
+    //       </p>
+    //     </a>
+    //   </div> */}
+    // </main>
+    
+    <main className={styles.page}>
+      <div className={styles.header}>
+        <h1>Shearly</h1>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className={styles.body}>
+        <h1>Welcome!</h1>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <p>View Salons near you</p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div> */}
-
-      <div>
-        <div className={styles.description}>
-          <h1 className={styles.center}>Welcome to Shearly</h1>
-        </div>
-
-        <div className={styles.grid}>
-          <a>
+        <div className={styles.loginGrid}>
+          <a className={styles.infoCard}>
             <h2> Provider</h2>
             <p>
               <button className={styles.button} role="button">Login</button>
@@ -105,7 +111,7 @@ export default function Home() {
             </p> 
           </a>
 
-          <a>
+          <a className={styles.infoCard}>
             <h2>Customer</h2>
             <p>
               <button className={styles.button} role="button">Login</button>
@@ -113,8 +119,7 @@ export default function Home() {
             </p>
           </a>
         </div>
-        
-      </div>
+      </div> 
     </main>
   );
 }
