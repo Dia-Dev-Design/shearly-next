3097e1c899fba6066720bbeae710c1a917f6dad1
Luis Ortiz <luis.ortiz7905@gmail.com>
2024-02-18 19:05:38 -0400

    Created sign up and sign in form

---
+++ app/signup/provider/page.tsx
@@ -0,0 +1,100 @@
+"use client"
+import styles from "./page.module.css";
+import React, { useState } from "react"
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
+
+export default function Provider() {
+  const [signup, setSignup] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        password: ""
+  });
+
+  const handleInput = (e) => {
+    const fieldName = e.target.name;
+    const fieldValue = e.target.value;
+
+    setSignup((prevState) => ({
+      ...prevState,
+      [fieldName]: fieldValue
+    }));
+  }
+
+  const submitForm = (e) => {
+    // We don't want the page to refresh
+    e.preventDefault()
+
+    const formURL = e.target.action
+    const data = new FormData()
+
+    // Turn our formData state into data we can use with a form submission
+    Object.entries(signup).forEach(([key, value]) => {
+      data.append(key, value);
+    })
+
+    // POST the data to the URL of the form
+    fetch(formURL, {
+      method: "POST",
+      body: data,
+      headers: {
+        'accept': 'application/json',
+      },
+    }).then((response) => response.json())
+    .then((data) => {
+      setSignup({
+        name: "",
+        email: "",
+        phone: "",
+        password: ""
+      })
+    })
+  }
+
+  return (
+    <main className={styles.main}>
+      <div>
+        <h1>Sign Up as Provider</h1>
+
+        <form method="POST" action="#mongooseEmail" onSubmit={submitForm} className={styles.form}>
+          <div className={styles.div}>
+            <label>Name</label>
+            <input type="text" name="name" onChange={handleInput} value={signup.name} />
+          </div>
+
+          <div className={styles.div}>
+            <label>Email</label>
+            <input type="text" name="email" onChange={handleInput} value={signup.email} />
+          </div>
+
+          <div className={styles.div}>
+            <label>Phone</label>
+            <PhoneInput country={'us'} value={signup.phone} onChange={phone => this.setState({ phone })} /> 
+          </div> 
+
+          <div className={styles.div}>
+            <label>Password</label>
+            <input 
+                type="password" 
+                name="password" 
+                onChange={handleInput} 
+                value={signup.password} 
+                pattern="[a-z]{0,9}" //add if possible or wanted {#@$!}
+                title="Password should be digits (0 to 9) or alphabets (a to z)."
+            />
+          </div>
+
+          <div className={styles.div}>
+            <label>Re-Enter Password</label>
+            <input type="password" name="re-enter-password" onChange={handleInput} value={signup.password}/>
+          </div>
+
+          <button type="submit">Sign Up</button>
+        </form>
+
+        <button>Cancel</button>
+      </div>
+    </main>
+  );
+}
