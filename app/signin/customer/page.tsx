a0e8b6509638fd598a9d5fe83b6961ae3d8d0412
Luis Ortiz <luis.ortiz7905@gmail.com>
2024-02-19 15:12:19 -0400

    Created sign up and sign in form for customer

---
+++ app/signin/customer/page.tsx
@@ -0,0 +1,78 @@
+"use client"
+import styles from "./page.module.css";
+import React, { useState } from "react"
+
+export default function Customer() {
+  const [login, setLogin] = useState({
+        email: "",
+        password: ""
+  });
+
+  const [loginSuccess, setLoginSuccess] = useState(false)
+
+  const handleInput = (e) => {
+    const fieldName = e.target.name;
+    const fieldValue = e.target.value;
+
+    setLogin((prevState) => ({
+      ...prevState,
+      [fieldName]: fieldValue
+    }));
+  }
+
+  const submitForm = (e) => {
+    // We don't want the page to refresh
+    e.preventDefault()
+
+    const formURL = e.target.action
+    const data = new FormData()
+
+    // Turn our formData state into data we can use with a form submission
+    Object.entries(login).forEach(([key, value]) => {
+      data.append(key, value);
+    })
+
+    // POST the data to the URL of the form
+    fetch(formURL, {
+      method: "GET",
+      body: data,
+      headers: {
+        'accept': 'application/json',
+      },
+    }).then((response) => response.json())
+    .then((data) => {
+      setLogin({
+        email: "",
+        password: ""
+      })
+
+      setLoginSuccess(true)
+    })
+  }
+
+  return (
+    <main className={styles.main}>
+      <div>
+        <h1>Sign In as Customer</h1>
+
+        {loginSuccess ?
+            <h1>Signed In</h1>
+        :
+        <form method="GET" action="#mongooseEmail" onSubmit={submitForm} className={styles.form}>
+          <div className={styles.field}>
+            <label>Email:</label>
+            <input type="text" name="email" onChange={handleInput} value={login.email} placeholder="..." />
+          </div>
+
+          <div className={styles.field}>
+            <label>Password:</label>
+            <input type="password" name="password" onChange={handleInput} value={login.password} placeholder="..." />
+          </div>
+
+          <button type="submit">Sign In</button>
+        </form>
+      }
+      </div>
+    </main>
+  );
+}
---
+++ app/signup/customer/page.tsx
@@ -0,0 +1,101 @@
+"use client"
+import styles from "./page.module.css";
+import React, { useState } from "react"
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
+
+export default function Customer() {
+  const [signup, setSignup] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        password: ""
+  });
+
+  const handleInput = (e) => {
+    const fieldName = e.target.name;
+    const fieldValue = e.target.value;
+
+    setSignup((prevState) => ({
+      ...prevState,
+      [fieldName]: fieldValue
+    }));
+  }
+
+  const submitForm = (e) => {
+    // We don't want the page to refresh
+    e.preventDefault()
+
+    const formURL = e.target.action
+    const data = new FormData()
+
+    // Turn our formData state into data we can use with a form submission
+    Object.entries(signup).forEach(([key, value]) => {
+      data.append(key, value);
+    })
+
+    // POST the data to the URL of the form
+    fetch(formURL, {
+      method: "POST",
+      body: data,
+      headers: {
+        'accept': 'application/json',
+      },
+    }).then((response) => response.json())
+    .then((data) => {
+      setSignup({
+        name: "",
+        email: "",
+        phone: "",
+        password: ""
+      })
+    })
+  }
+
+  return (
+    <main className={styles.main}>
+      <div>
+        <h1>Sign Up as Customer</h1>
+
+        <form method="POST" action="#mongooseEmail" onSubmit={submitForm} className={styles.form}>
+          <div className={styles.field}>
+            <label>Name:</label>
+            <input type="text" name="name" onChange={handleInput} value={signup.name} placeholder="..." />
+          </div>
+
+          <div className={styles.field}>
+            <label>Email:</label>
+            <input type="text" name="email" onChange={handleInput} value={signup.email} placeholder="..." />
+          </div>
+
+          <div className={styles.field}>
+            <label>Phone:</label>
+            <PhoneInput country={'us'} value={signup.phone} onChange={phone => this.setSignup({ phone })} placeholder="..." /> 
+          </div> 
+
+          <div className={styles.field}>
+            <label>Password:</label>
+            <input 
+                type="password" 
+                name="password" 
+                onChange={handleInput} 
+                value={signup.password} 
+                pattern="[a-z]{0,9}" //add if possible or wanted {#@$!}
+                title="Password should be digits (0 to 9) or alphabets (a to z)."
+                placeholder="..."
+            />
+          </div>
+
+          <div className={styles.field}>
+            <label>Re-Enter Password:</label>
+            <input type="password" name="re-enter-password" onChange={handleInput} value={signup.password} placeholder="..." />
+          </div>
+
+          <button type="submit">Sign Up</button>
+        </form>
+
+        <button>Cancel</button>
+      </div>
+    </main>
+  );
+}
