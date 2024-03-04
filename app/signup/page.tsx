6dc50b84d4841cb3e61ac6972369b7376cef1687
Luis Ortiz <luis.ortiz7905@gmail.com>
2024-03-04 19:26:40 -0400

    Put both provider and customer sign in and sign up in one page

---
+++ app/signup/page.tsx
@@ -0,0 +1,58 @@
+"use client"
+import styles from "./page.module.css";
+import React, { useState } from "react";
+import ClientForm from "./Components/ClientSignupForm";
+import ProviderForm from "./Components/ProviderSignupForm";
+
+export default function SignUp() {
+  const [radio, setRadio] = useState(false);
+
+  const handleInput = (value) => {
+    setRadio(value);
+  }
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.body}> {!radio ?
+        <div>
+            <div className={styles.wrapper}>
+                <input type="radio" name="select" id={styles.option1} checked={!radio} onChange={() => handleInput(false)}/>
+                <input type="radio" name="select" id={styles.option2} checked={radio} onChange={() => handleInput(true)}/>
+                <label htmlFor="option1" className={`${styles.option} ${styles.option1}`}>
+                    <div className={styles.dot}></div>
+                    <span>Provider</span>
+                </label>
+                <label htmlFor="option2" className={`${styles.option} ${styles.option2}`}>
+                    <div className={styles.dot}></div>
+                    <span>Customer</span>
+                </label>
+            </div>
+
+            <div>
+                <ProviderForm />
+            </div>
+        </div>
+        :
+        <div>
+            <div className={styles.wrapper}>
+                <input type="radio" name="select" id={styles.option1} checked={!radio} onChange={() => handleInput(false)}/>
+                <input type="radio" name="select" id={styles.option2} checked={radio} onChange={() => handleInput(true)}/>
+                <label htmlFor="option1" className={`${styles.option} ${styles.option1}`}>
+                    <div className={styles.dot}></div>
+                    <span>Provider</span>
+                </label>
+                <label htmlFor="option2" className={`${styles.option} ${styles.option2}`}>
+                    <div className={styles.dot}></div>
+                    <span>Customer</span>
+                </label>
+            </div>
+
+            <div>
+                <ClientForm />
+            </div>
+        </div>
+        }
+      </div>
+    </main>
+  );
+}
