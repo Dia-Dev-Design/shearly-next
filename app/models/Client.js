81bbce5e02bf05037048246643e5a2c864d2e3a6
Luis Ortiz <luis.ortiz7905@gmail.com>
2024-03-10 14:55:08 -0400

    Created Client and Provider models

---
+++ app/models/Client.js
@@ -0,0 +1,24 @@
+const { model, Schema } = require('mongoose');
+
+const clientSchema = new Schema(
+    {
+        image: { type: String, default: 'https://cvhrma.org/wp-content/uploads/2015/07/default-profile-photo.jpg' },
+        name: { type: String, required: true, trim: true },
+        email: { type: String, required: true, trim: true },
+        password: { type: String, required: true },
+        phone: { type: String, default: '' },
+        address: { type: Object },
+        specialCare: { type: Object },
+        appointment: [
+            { type: Schema.Types.ObjectId, ref: 'Appointment' }
+        ],
+        transaction: [
+            { type: Schema.Types.ObjectId, ref: 'Transaction' }
+        ]
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = model('Client', clientSchema);
