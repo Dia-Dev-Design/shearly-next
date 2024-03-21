656463afc0fb0bd9a77a896b1041892a875d3901
alextor26 <alextor2625@gmail.com>
2024-03-21 02:41:08 -0400

    Completed Service Model

--- app/models/Service.js
+++ app/models/Service.js
@@ -1,11 +1,10 @@
-const {Schema, model} = require('mongoose');
-
+const { Schema, model } = require("mongoose");
 
 const serviceSchema = new Schema({
-   name: {type: String, required: true, trim: true},
-   price: {type: Number, default: 0, min: 0},
-   image: {type: String, default: "", trim: true},
-   description: {type: String, default: "", trim: true}
-})
+  name: { type: String, required: true, trim: true },
+  price: { type: Number, default: 0, min: 0 },
+  image: { type: String, default: "", trim: true },
+  description: { type: String, default: "", trim: true },
+});
 
-module.exports(serviceSchema, "Service")
+module.exports = model("Service", serviceSchema);
