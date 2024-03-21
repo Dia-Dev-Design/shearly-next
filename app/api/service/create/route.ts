208417a5c7001f2960b44146634111a55d26185b
alextor26 <alextor2625@gmail.com>
2024-03-21 14:09:12 -0400

    Completed Create Service Route

--- app/api/service/create/route.ts
+++ app/api/service/create/route.ts
@@ -1,24 +1,70 @@
-import { NextRequest } from "next/server";
 import dbConnect from "@/app/lib/dbConnect";
+import mongoose from "mongoose";
 import Service from "@/app/models/Service";
 
-export async function POST(req:Request){
-    try {
-        const body = await req.json();
-        await dbConnect();
-        if(!("name" in body) || !body.name){
-            console.error("\nError: Service name is Required!")
-            return Response.json({message: "Service name is Required!"}, {status: 400})
-        }
-        const findService = await Service.findOne({name: body.name})
-        if(findService){
-            console.error(`\nError: Service with name: ${body.name} already exists!`)
-            return Response.json({message: `Service with name: ${body.name} already exists!`}, {status: 400})
-        }
-        const createService = Service.create({...body});
-        console.log("Success!");
-        return Response.json({message: "OK", service: createService}, {status: 201})
-    } catch (error) {
-        
+type PostBody = {
+  name: string;
+  price?: number;
+  image?: string;
+  description?: string;
+};
+
+interface IService {
+  name: string;
+  price?: number;
+  image?: string;
+  description?: string;
+}
+
+export async function POST(req: Request) {
+  try {
+    const body: PostBody = await req.json();
+    await dbConnect();
+    if (!body.name) {
+      console.error("\nError: Service name is Required!");
+      return Response.json(
+        { message: "Service name is Required!" },
+        { status: 400 }
+      );
     }
+    const findService: IService| null = await Service.findOne({ name: body.name });
+    if (findService) {
+      console.error(`\nError: Service with name: ${body.name} already exists!`);
+      return Response.json(
+        { message: `Service with name: ${body.name} already exists!` },
+        { status: 400 }
+      );
+    }
+    const createService = await Service.create({ ...body });
+    console.log("Success!");
+    return Response.json(
+      { message: "OK", service: createService },
+      { status: 201 }
+    );
+  } catch (error: any) {
+    if(error.name === 'MongoNetworkError'){
+        console.error(`\nError: Database is offline!\n${error.message}`)
+        return Response.json({message: "Database is offline!"}, {status: 500})
+    }
+    if (error instanceof mongoose.Error.ValidationError) {
+      console.error(
+        "\nMongoose Schema Validation Error ==> ",
+        error.message
+      );
+      return Response.json(
+        {
+          message: "Name is required to create a Service!",
+        },
+        { status: 400 }
+      );
+    } else {
+      console.error("\nInternal Server Error! Error:", error.message);
+      return Response.json(
+        {
+          message: "Internal Server Error!",
+        },
+        { status: 500 }
+      );
+    }
+  }
 }
